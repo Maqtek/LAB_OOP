94c25bc05f498a03f19355000c17a5dcce4c869e
Matvei <matvei.v06@gmail.com>
2026-04-26 23:38:06 +0300

    Добавлен задание 1 - Вектор

---
+++ Lab1/Vector/MyVector.cs
@@ -0,0 +1,50 @@
+﻿namespace Vector
+{
+    class MyVector
+    {
+        private readonly double DirectionX, DirectionY;
+
+        public MyVector(double x, double y)
+        {
+            DirectionX = x;
+            DirectionY = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({DirectionX}, {DirectionY})";
+        }
+
+        public static MyVector operator +(MyVector first, MyVector other)
+        {
+            return new MyVector(first.DirectionX + other.DirectionX, first.DirectionY + other.DirectionY);
+        }
+
+        public static MyVector operator -(MyVector first, MyVector other)
+        {
+            return new MyVector(first.DirectionX - other.DirectionX, first.DirectionY - other.DirectionY);
+        }
+
+        public static double operator *(MyVector first, MyVector other)
+        {
+            return first.DirectionX * other.DirectionX + first.DirectionY * other.DirectionY;
+        }
+
+        public static double operator !(MyVector vector)
+        {
+            return Math.Sqrt(vector.DirectionX * vector.DirectionX + vector.DirectionY * vector.DirectionY);
+        }
+
+        public static bool operator ==(MyVector first, MyVector other)
+        {
+            return first.DirectionX == other.DirectionX && first.DirectionY == other.DirectionY;
+        }
+
+        public static bool operator !=(MyVector first, MyVector other)
+        {
+            return first.DirectionX != other.DirectionX || first.DirectionY != other.DirectionY;
+        }
+
+    }
+}
+
